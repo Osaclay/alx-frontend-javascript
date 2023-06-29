3778af7064b767df49e3e83d06159ba9989b2d1b
Osaclay <osaosun@gmail.com>
2023-06-29 19:02:57 +0000

    ES6_classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,6 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom';
 
-function initializeRooms() {
-  const room1 = new ClassRoom(19);
-  const room2 = new ClassRoom(20);
-  const room3 = new ClassRoom(34);
-
-  return [room1, room2, room3];
+export default function initializeRooms() {
+  const rooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return rooms;
 }
-
-export default initializeRooms;
