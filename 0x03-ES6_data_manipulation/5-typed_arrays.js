6b5667fd2f7ce5f629dfc939c22e2accf1a8f141
Osaclay <osaosun@gmail.com>
2023-07-04 19:15:14 +0000

    ES6_data_manipulation

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,10 @@
-function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
-    throw new Error("Position outside range");
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
   }
 
   const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
-
-  int8View[position] = value;
-
-  return int8View;
+  const dataView = new DataView(buffer);
+  dataView.setInt8(position, value);
+  return dataView;
 }
-
-export default createInt8TypedArray;
